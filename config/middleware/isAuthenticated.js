073445c0518f21a3a523f0531c50ff72eafee603
Alisha Panday <panday.alisha90@gmail.com>
2021-03-03 21:02:15 +0800

    authentication middleware added and is working

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,16 +1,13 @@
-//This is middleware for restricting routes a user is not allowed to visit if not logged in
-//commented out for now so that it does not throw any errors
-
-
-
-// module.exports = function(req, res, next) {
-//   // If the user is logged in, continue with the request to the restricted route
-//   if (req.user) {
-//     return next();
-//   }
-
-//   // If the user isn't logged in, redirect them to the login page
-//   return res.redirect("/frontPage");
-// };
-
+// This is middleware for restricting routes a user is not allowed to visit if not logged in
+module.exports = function(req, res, next) {
+    // If the user is logged in, continue with the request to the restricted route
+    if (req.user) {
+      return next();
+    }
+  
+    // If the user isn't logged in, redirect them to the login page
+    return res.render('login', {
+      style: "login.css"
+    })
+  };
   
