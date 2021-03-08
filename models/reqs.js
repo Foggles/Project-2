301c702792807887fb45b8a55208bd286e2a122c
Hamish Isaacs <hamisaacs@gmail.com>
2021-03-08 19:57:28 +0800

    My changes.

---
+++ models/reqs.js
@@ -0,0 +1,44 @@
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+// Creating our User model
+module.exports = function (Sequelize, DataTypes) {
+ const reqs = Sequelize.define(
+  'reqs',
+  {
+   // The email cannot be null, and must be a proper email before creation
+   id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+   },
+   // The password cannot be null
+   todoId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+   },
+   todoTitle: {
+    type: DataTypes.STRING,
+    allowNull: true,
+   },
+   from: {
+    type: DataTypes.STRING,
+    allowNull: true,
+   },
+   to: {
+    type: DataTypes.STRING,
+    allowNull: true,
+   },
+   msg: {
+    type: DataTypes.STRING,
+    allowNull: true,
+   },
+   state: {
+    type: DataTypes.STRING,
+    allowNull: true,
+   },
+  },
+  {
+   timestamps: false,
+  }
+ );
+ return reqs;
+};
--- server.js
+++ server.js
@@ -1,50 +1,68 @@
 // Requiring necessary npm packages
-const express = require("express");
-const session = require("express-session");
-const exphbs = require('express-handlebars');
+const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
 // Requiring passport as we've configured it
-const passport = require("./config/passport");
+const passport = require('./config/passport')
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8000;
-const db = require("./models");
+const PORT = process.env.PORT || 8000
+const db = require('./models')
 
 // Creating express app and configuring middleware needed for authentication
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+const app = express()
+
+app.use(session({
+
+  secret: 'key',
+
+
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  }
+
+}))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
+  session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+  })
+)
 
 
 // Set Handlebars.
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-
-//Passport initialisation 
-app.use(passport.initialize());
-app.use(passport.session());
+//Passport initialisation
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Requiring our routes
-require("./controllers/html-routes.js")(app);
-require("./controllers/api-routes.js")(app);
+require('./controllers/html-routes.js')(app)
+require('./controllers/api-routes.js')(app)
 
 // Set Handlebars.
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({force:true}).then(() => {
+db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
       PORT,
       PORT
-    );
-  });
+    )
+  })
 })
 
