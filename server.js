5655c700945c2464e6fbec5c235f74480f95e638
Herve <hmalvina@yahoo.com>
2021-03-15 11:07:41 +0800

    gmail functionality added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Requiring necessary npm packages
+const nodemailer = require('nodemailer');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
@@ -50,6 +51,29 @@ app.use(apiRouter);
 // Set Handlebars.
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+//configuring the emmail service
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'volunteersteam2021@gmail.com',
+        pass: 'backend-warriors'
+    }
+});
+
+let mailOptions ={
+    from: '',
+    to: 'volunteersteam2021@gmail.com',
+    subject: 'I need help please',
+    text: 'It works'
+};
+transporter.sendMail(mailOptions, function(err,data){
+    if (err) {
+        console.log('Error occurs');
+    }else {
+        console.log('Email sent!!');
+    }
+});
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync({ force: false }).then(() => {
